cc9bbc7371f08b22d82413732d6c2f86e563e15b
Aman Sharma <amankumarsharma1122@gmail.com>
2024-02-28 15:47:50 +0530

    github first full deloyment

---
+++ Backend/src/main/java/com/axmayn/PingPongController.java
@@ -0,0 +1,13 @@
+package com.axmayn;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class PingPongController{
+
+    record PingPong(String result){};
+
+    @GetMapping("/ping")
+    PingPong getPingPong(){
+        return new PingPong("Pong");
+    }
+}
